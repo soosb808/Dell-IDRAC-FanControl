494ae07ee566baca2b79a40ce0b47c0389383e35
Eric <eric.wong.cug@gmail.com>
2024-10-14 03:43:12 +0800

    improve

--- adaptivefancontrol.sh
+++ adaptivefancontrol.sh
@@ -7,46 +7,41 @@ function set_speed {
   ipmitool -I lanplus -H $IDRAC_HOST -U $IDRAC_USER -P $IDRAC_PW raw 0x30 0x30 0x02 0xff $HEX > /dev/null
 }
 
+while true; do
 TEMP=$(get_temp)
     if [ "$TEMP" -ge 0 ] && [ "$TEMP" -lt 35 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 10%"
-        HEX=0x0a
-        set_speed
+        HEX=0x0a 
     elif [ "$TEMP" -ge 35 ] && [ "$TEMP" -lt 40 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 17%"
         HEX=0x11
-        set_speed
     elif [ "$TEMP" -ge 40 ] && [ "$TEMP" -lt 45 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 25%"
         HEX=0x19
-        set_speed
     elif [ "$TEMP" -ge 45 ] && [ "$TEMP" -lt 50 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 30%"
         HEX=0x1e
-        set_speed
     elif [ "$TEMP" -ge 50 ] && [ "$TEMP" -lt 55 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 40%"
         HEX=0x28
-        set_speed
     elif [ "$TEMP" -ge 55 ] && [ "$TEMP" -lt 60 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 50%"
         HEX=0x32
-        set_speed
     elif [ "$TEMP" -ge 60 ] && [ "$TEMP" -lt 70 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 70%"
         HEX=0x46
-        set_speed
     elif [ "$TEMP" -ge 70 ] && [ "$TEMP" -lt 80 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 90%"
         HEX=0x5a
-        set_speed
     elif [ "$TEMP" -ge 80 ] && [ "$TEMP" -le 100 ]; then
         echo "Exhaust Temperature is $TEMP : Setting fan speed to 100%"
         HEX=0x64
-        set_speed
     else
         echo "Temperature $TEMP: Was not able to get proper tempature"
 fi
 
+set_speed
+
 sleep "$CHECK_INTERVAL"s
-exec "/opt/adaptivefancontrol.sh"
+
+done
--- dockerfile
+++ dockerfile
@@ -1,11 +1,13 @@
-FROM alpine:edge
+FROM alpine:latest
 
-RUN apk --no-cache add ipmitool apk-cron
+RUN apk --no-cache add ipmitool apk-cron && \
+    rm -rf /var/cache/apk/*
 
 COPY crontab /etc/cron.d/fan-control
 COPY adaptivefancontrol.sh /opt/adaptivefancontrol.sh
-RUN chmod 0777 /etc/cron.d/fan-control && \
-    chmod 0777 /opt/*.sh && \
-    /usr/bin/crontab /etc/cron.d/fan-control
 
-CMD [/opt/adaptivefancontrol.sh && crond]
+RUN chmod 0644 /etc/cron.d/fan-control && \
+    chmod 0755 /opt/adaptivefancontrol.sh && \
+    crontab /etc/cron.d/fan-control
+
+CMD ["/opt/adaptivefancontrol.sh", "&&", "crond"]
